bc5dd6084bb013bf1d92316d74102d33e5fde6be
gitSTRanger <95970001+gitSTRanger@users.noreply.github.com>
2025-01-31 09:16:26 +0300

    PZ 6
    completed PZ 6

--- PZ-6/PZ_6_1.py
+++ PZ-6/PZ_6_1.py
@@ -2,18 +2,18 @@
 Дан список размера N и целые числа K и L (1 < K < L < N). Найти сумму всех
 элементов списка, кроме элементов с номерами от K до L включительно
 '''
-"""
+
 import random
 
 K = random.randint(2, 5)
-L = random.randint(K, K+5)
-N = random.randint(L,L+5)
+L = random.randint(K, (K+5))
+N = random.randint(L,(L+5))
 
 
-numList = [int]
+numList = []
 
-def WriteList(Flist_1: list[int] = int):
-    for i in range(N):
+def WriteList(Flist_1):
+    for i in range(N +1):
         Flist_1.append(random.randint(0,N))
     return Flist_1
 
@@ -21,15 +21,18 @@ def WriteList(Flist_1: list[int] = int):
 list = WriteList(numList)
 
 def calculateSum(Flist_2):
-    sum = 0
-    firstPart = Flist_2[0:K-1]
-    secondPart = Flist_2[L:-1]
-    
+    sum_ = 0
+    firstPart = Flist_2[0:K]
+    secondPart = Flist_2[L+1:]
+
+    print(firstPart)
+    print(secondPart)
+
     for elem in firstPart:
-        sum += elem
+        sum_ += elem
     for elem in secondPart:
-        sum += elem
-    return sum
+        sum_ += elem
+    return sum_
         
 
 
@@ -37,5 +40,6 @@ def calculateSum(Flist_2):
     
 
 print(f'K:{K} L:{L} N:{N}')
-print("сумма:", calculateSum(numList))
-"""
+print(list)
+print("сумма:", calculateSum(list))
+
